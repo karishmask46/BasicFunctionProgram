064e8fc2a70142bb16787bc50889c9dbca1b4087
karishmask46 <karishmask.15999@gmail.com>
2022-06-09 11:14:53 +0530

    [Karishma]calculating power of two

---
+++ BasicFunctionProgram/BasicFunctionProgram/Power_Of_Two.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicFunctionProgram
+{
+    internal class Power_Of_Two
+    {
+        public void PowerOfTwo()
+        {
+            Console.WriteLine("please enter a number ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            while (number < 0 || number >= 31)
+            {
+                Console.WriteLine("please enter a valid number: ");
+                number = Convert.ToInt32(Console.ReadLine());
+
+            }
+            for (int i = 1; i <= number; i++)
+            {
+                Console.WriteLine(Math.Pow(2, i));
+
+            }
+        }
+    }
+}
+
--- BasicFunctionProgram/BasicFunctionProgram/Program.cs
+++ BasicFunctionProgram/BasicFunctionProgram/Program.cs
@@ -7,7 +7,7 @@
             Console.WriteLine("Enter a number for relative problem.");
             Console.WriteLine("1 for a flip coin and print percentage of Heads and  Tails");
             Console.WriteLine("2 for a leap year");
- 
+            Console.WriteLine("3 for a power of 2");
 
             int function = Convert.ToInt32(Console.ReadLine());
 
@@ -23,9 +23,13 @@
                     LeapYear leapYear = new LeapYear();
                     leapYear.Leapyear();
                     break;
-                
+                case 3:
+                    Power_Of_Two Power = new Power_Of_Two();
+                    Power.PowerOfTwo();
+                    break;
 
-       
+
+
 
             }
         }
